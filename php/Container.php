e799a2a3ce900db7ccc3529b78837ad80b191016
Juan Sebastian Scatularo <sebastianscatularo@gmail.com>
2013-09-03 20:02:01 -0400

    Sebastian Scatularo - fixed wrong folder

--- php/Container.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Sofia;
-
-interface Container {
-    public function register($id, $className, $methodFactory = null);
-    public function inject($id, array $dependencies);
-    public function get($id);
-}
